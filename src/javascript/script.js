50c44f24385113aeb68a9709c81124534b4d420d
VICTOR FRANÇA <162994587+uviitinho@users.noreply.github.com>
2024-12-04 19:16:03 -0300

    script

---
+++ src/javascript/script.js
@@ -0,0 +1,11 @@
+document.querySelector('#search').addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const cityName = document.querySelector('#city_name').value;
+
+    if (!cityName) {
+        return alert('Você precisa digitar uma cidade...');
+    }
+
+    console.log('Cidade digitada:', cityName);
+});
